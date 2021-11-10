baf9ae06524360bd5a4ce4113ba669ac6ea0f6c6
Narmada01 <narmadacrish@gmail.com>
2021-11-10 14:21:08 +0000

    "changes made"

--- src/main/java/PageObject/searchPOM.java
+++ src/main/java/PageObject/searchPOM.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class searchPOM {
+    String str= new String();
     public searchPOM(WebDriver driver){
         PageFactory.initElements(driver, this);
     }
@@ -26,7 +27,7 @@ public class searchPOM {
     }
     public void checkTitleName(String expectedName)
     {
-        Assert.assertTrue(titleName.getText().contains(expectedName));
+        Assert.assertTrue(titleName.getText().contains(expectedName) );
     }
 
 }
